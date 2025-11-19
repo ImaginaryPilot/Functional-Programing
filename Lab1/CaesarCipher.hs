6150a21d2d4f70d785f9f8d1ff543f9f20bbf05b
Kailu <kailupucciarelli@yahoo.com>
2025-11-19 13:59:14 +0100

    Caesar Cipher Complete

--- Lab1/CaesarCipher.hs
+++ Lab1/CaesarCipher.hs
@@ -2,11 +2,28 @@ import Data.Char
 
 encode::Int->Char->[Char]
 encode n c 
-    | c >= 'A' && c <= 'Z' = [chr ((mod (ord 'Z' - ord 'A' - n + 1) 26) + ord 'A')]
+    | c >= 'A' && c <= 'Z' = [chr ((mod (ord c - ord 'A' - n + 26) 26)  + ord 'A')]
     | otherwise = [c]
 
 cipherEncode::Int->String->String
 cipherEncode n xs = h n xs "" n
     where
         h n [] a k = a
-        h n xs a k = h n (tail(xs)) (a++(encode k (head(xs)))) (mod (k+n) 26)
+        h n xs a k 
+            | c >= 'A' && c <= 'Z' = h n (tail(xs)) (a++(encode k c)) (mod (k+n) 26)
+            | otherwise = h n (tail(xs)) (a++(encode k c)) k
+            where c = head(xs)
+
+decode::Int->Char->[Char]
+decode n c 
+    | c >= 'A' && c <= 'Z' = [chr ((mod (ord c - ord 'A' + n + 26) 26)  + ord 'A')]
+    | otherwise = [c]
+
+cipherDecode::Int->String->String
+cipherDecode n xs = h n xs "" n
+    where
+        h n [] a k = a
+        h n xs a k 
+            | c >= 'A' && c <= 'Z' = h n (tail(xs)) (a++(decode k c)) (mod (k+n) 26)
+            | otherwise = h n (tail(xs)) (a++(decode k c)) k
+            where c = head(xs)    
