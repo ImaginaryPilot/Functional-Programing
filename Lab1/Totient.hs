484b595cb0d63ca8d4d22fbd1cadfcad55cda291
Kailu <kailupucciarelli@yahoo.com>
2025-11-19 18:50:22 +0100

    Totient Incomplete

--- Lab1/Totient.hs
+++ Lab1/Totient.hs
@@ -8,11 +8,26 @@ isPrime n
     | even n = False
     | otherwise = null [d | d<-[3,5..floor(sqrt(fromIntegral n))], mod n d == 0] 
 
+factors :: Int -> [Int]
+factors n = [d | d <- [1 .. floor(sqrt(fromIntegral n))], mod n d == 0]
+
 distinctPrimes:: Integer -> [Integer]
-distinctPrimes n = [p | p <- divisors (floor(sqrt(fromIntegral n))), isPrime p]
+distinctPrimes n = [p | p <- divisors n, isPrime p]
+
+divide::Integer->Integer->(Integer, Integer)
+divide m n = h m n 0
+    where
+        h m n count
+            | mod m n == 0 = h (div m n) n (count+1)
+            | otherwise = (m, count)
 
 totient::Integer->Integer
 totient n = h n (distinctPrimes n) n
     where
-        h n [] k = floor(k)
-        h n xs k = h n (tail(xs)) (k*(1-(1/head(xs)))) 
+        h n _ k = k
+        h n xs k
+            | mod j n == 0 = h (fst(i)) (tail(xs)) (k^(snd i)*(j - 1))
+            | otherwise = h n (tail(xs)) k
+            where 
+                j = head(xs)
+                i = divide n j
