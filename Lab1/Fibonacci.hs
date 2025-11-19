923eafc47b661eebe9bfeba6525aee17bf394a43
Kailu <kailupucciarelli@yahoo.com>
2025-11-19 14:32:40 +0100

    Incomplete Totient

--- Lab1/Fibonacci.hs
+++ Lab1/Fibonacci.hs
@@ -3,3 +3,11 @@ fib n = f n 1 2
     where
         f 0 a b = a
         f i a b = f (i-1) b (a+b)
+
+factor::Integer->Integer
+factor n 
+    | n <= fib n
+    | otherwise = 
+
+numberOfFiboSums::Integer->Integer
+numberOfFiboSums n = 
--- Lab1/Totient.hs
+++ Lab1/Totient.hs
@@ -1,11 +1,18 @@
-divisors:: Integer->[Integer]
+divisors:: Integer -> [Integer]
 divisors n = 2:[3,5..n]
 
-factors::Integer->Integer
-factors n
-    | even n = factors (n/2)
-    
-    [d | d <- [2 .. n]]
+isPrime:: Integer -> Bool
+isPrime n 
+    | n < 2 = False
+    | n == 2 = True
+    | even n = False
+    | otherwise = null [d | d<-[3,5..floor(sqrt(fromIntegral n))], mod n d == 0] 
+
+distinctPrimes:: Integer -> [Integer]
+distinctPrimes n = [p | p <- divisors (floor(sqrt(fromIntegral n))), isPrime p]
 
 totient::Integer->Integer
-totient n = 
+totient n = h n (distinctPrimes n) n
+    where
+        h n [] k = floor(k)
+        h n xs k = h n (tail(xs)) (k*(1-(1/head(xs)))) 
