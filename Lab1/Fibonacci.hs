8470c604599f7d250fbbee9a3b35e225a8e56431
Kailu <kailupucciarelli@yahoo.com>
2025-11-22 12:48:29 +0100

    Fibonacci Complete

--- Lab1/Fibonacci.hs
+++ Lab1/Fibonacci.hs
@@ -1,13 +1,15 @@
-fib::Integer->Integer
-fib n = f n 1 2
+fib::Integer->[Integer]
+fib n = f 1 2 []
     where
-        f 0 a b = a
-        f i a b = f (i-1) b (a+b)
-
-factor::Integer->Integer
-factor n 
-    | n <= fib n
-    | otherwise = 
+        f a b xs
+            | b > n = xs++[a]
+            | otherwise = f b (a+b) (xs++[a])
 
 numberOfFiboSums::Integer->Integer
-numberOfFiboSums n = 
+numberOfFiboSums n = h n (fib n)
+    where
+        h 0 _ = 1
+        h _ [] = 0
+        h k (f:fs)
+            | f > k = 0
+            | otherwise = (h (k - f) fs) + (h k fs)
